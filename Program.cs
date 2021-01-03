17671fce94d5b520406143f585caeea6ee37ff5f
Regis Leandro da Silva Ribeiro Magalhães <engenheiro.regis.magalhaes@gmail.com>
2021-01-03 03:36:01 -0200

    Este programa mostra a tabuada

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("-----------Tabuada-----------");
+            System.Console.WriteLine("Digite um número inteiro: ");
+            num = int.Parse(Console.ReadLine());
+            
+            for (int i = 1; i <= 10; i++)
+            {
+                System.Console.WriteLine("{0} x {1} = {2}", num, i, (num*i));
+            }
+        }
+    }
+}
